921db4c6c5b6f40851c21634fb124875b4c7f378
Adeleye Ayodeji <biodunhi@gmail.com>
2022-09-03 22:26:54 +0100

    Added a new file

---
+++ lib/flutter_text.dart
@@ -0,0 +1,51 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+void main() {
+  //runapp
+  runApp(const AdeEntryWidget());
+}
+
+//Entry Widget
+class AdeEntryWidget extends StatelessWidget {
+  const AdeEntryWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    //return materialApp
+    return MaterialApp(
+      title: "Ade Application",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: AdeHomePage(),
+    );
+  }
+}
+
+//AdeHomePage
+class AdeHomePage extends StatefulWidget {
+  const AdeHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<AdeHomePage> createState() => _AdeHomePageState();
+}
+
+class _AdeHomePageState extends State<AdeHomePage> {
+  //scaffoldkey
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Ade Application"),
+        ),
+        body: Text("Flutter Text"));
+  }
+}
