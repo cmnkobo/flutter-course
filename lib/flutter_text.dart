13aa427a993caf62c7e7c79e1fc1345906bde57f
Adeleye Ayodeji <biodunhi@gmail.com>
2022-09-05 17:49:44 +0100

    Added Flutter Text

--- lib/flutter_text.dart
+++ lib/flutter_text.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/src/foundation/key.dart';
@@ -46,6 +47,47 @@ class _AdeHomePageState extends State<AdeHomePage> {
         appBar: AppBar(
           title: Text("Ade Application"),
         ),
-        body: Text("Flutter Text"));
+        body: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.all(20),
+              height: 100,
+              child: Text(
+                "Welcome to AQskill fdklmkf dff dfdfd dfdf dfdf ere erere d fdf dfdfdfd dfd c vc v cvcv fdfd dfd er ere dfdf",
+                textDirection: TextDirection.ltr,
+                textAlign: TextAlign.center,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+                style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.purple,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2,
+                    wordSpacing: 3),
+              ),
+            ),
+            RichText(
+                text: TextSpan(
+              text: "Don't have an account?",
+              style: TextStyle(color: Colors.black, fontSize: 16),
+              children: [
+                WidgetSpan(
+                  child: Icon(Icons.unsubscribe_rounded),
+                ),
+                TextSpan(
+                    text: " Sign Up",
+                    style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold),
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () {
+                        print("Sign Up");
+                      })
+              ],
+            ))
+          ],
+        ));
   }
 }
