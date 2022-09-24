60004bf16f6399036400f5f363c424db3de180a8
Adeleye Ayodeji <biodunhi@gmail.com>
2022-09-24 11:54:16 +0100

    Added bottom bar

--- lib/home.dart
+++ lib/home.dart
@@ -19,12 +19,13 @@ class AdeEntryWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     //return materialApp
     return MaterialApp(
-      title: "Ade Application",
+      title: "News App",
       debugShowCheckedModeBanner: false,
-      //white background
       theme: ThemeData(
-        colorScheme:
-            ColorScheme.light(primary: Colors.white, onPrimary: Colors.black),
+        colorScheme: ColorScheme.light(
+          primary: Colors.white,
+          onPrimary: Colors.black,
+        ),
       ),
       home: AdeHomePage(),
     );
@@ -42,19 +43,20 @@ class AdeHomePage extends StatefulWidget {
 class _AdeHomePageState extends State<AdeHomePage> {
   //scaffoldkey
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  int pageIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title:
-              Text("News App", style: TextStyle(fontWeight: FontWeight.bold)),
+          title: Text(
+            "News App",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
           actions: [
-            //share
             IconButton(
               icon: Icon(Icons.share),
               onPressed: () {
-                //share
                 print("Share");
               },
             ),
@@ -66,180 +68,190 @@ class _AdeHomePageState extends State<AdeHomePage> {
             ),
             SizedBox(
               width: 10,
-            ),
+            )
           ],
         ),
+        backgroundColor: Colors.white,
         body: SingleChildScrollView(
           child: Column(
             children: [
               //header
               Container(
                 width: double.infinity,
-                //background with image opacity
+                height: 250,
+                //background image
                 decoration: BoxDecoration(
                   image: DecorationImage(
                     image: NetworkImage(
-                        "https://www.lindaikejisblog.com/photos/shares/thumbs/632a1c4d0947f.jpg"),
+                        "https://www.lindaikejisblog.com/photos/shares/thumbs/632cb2cdbdaa4.PNG"),
                     fit: BoxFit.cover,
                   ),
                 ),
-                height: 250,
                 child: Container(
-                    decoration: BoxDecoration(
+                  decoration: BoxDecoration(
                       gradient: LinearGradient(
-                        begin: Alignment.bottomCenter,
-                        end: Alignment.topCenter,
-                        colors: [
-                          Colors.black.withOpacity(1),
-                          Colors.black.withOpacity(.0),
-                        ],
-                      ),
-                    ),
-                    padding: EdgeInsets.all(15),
-                    child: Align(
+                    begin: Alignment.bottomRight,
+                    colors: [
+                      Colors.black.withOpacity(1),
+                      Colors.black.withOpacity(.0),
+                    ],
+                  )),
+                  padding: EdgeInsets.all(15),
+                  child: Align(
                       alignment: Alignment.bottomLeft,
                       child: Text(
-                        "Queen's funeral was watched by average audience of 26.2million on British TV stations",
-                        overflow: TextOverflow.ellipsis,
+                        "Atiku Abubakar's secretary, Abdullah Nyako is dead",
                         maxLines: 3,
-                        softWrap: true,
+                        overflow: TextOverflow.ellipsis,
                         style: TextStyle(
                             fontSize: 26,
                             color: Colors.white,
                             height: 1.2,
                             fontWeight: FontWeight.w900),
-                      ),
-                    )),
+                      )),
+                ),
               ),
 
               //body
-              for (var i = 0; i < 10; i++)
-                AdeNewsCard().getCard(
+              for (var i = 0; i < 3; i++)
+                AdeBlogList().getCard(
+                    date: "2021-09-10",
+                    thumbnail:
+                        "https://www.lindaikejisblog.com/photos/shares/thumbs/632ca30dd14bc.PNG",
                     title:
-                        "Nigerian couple who met on Twitter celebrate 8th wedding anniversary",
+                        "Nigerian prophet sentenced to 7 years in jail for defrauding church member of N59.7 million",
                     description:
-                        "A Nigerian couple, Tunde and Foluso Leye, who met on Twitter, celebrated their 8th wedding anniversary on Tuesday, September 20.   Tunde had tweeted about bad network on April 2013",
-                    imageUrl:
-                        "https://www.lindaikejisblog.com/photos/shares/thumbs/632a11d666321.jpg",
-                    author: "Linda Ikeji",
-                    date: "20/09/22",
-                    comments: "2"),
+                        "An Ikeja Special Offences Court in Lagos has sentenced a Lagos-based prophet, Cletus Ilongwo, to seven years imprisonment for defrauding his church member of N59.7 million.   Recall tha",
+                    author: "Linda Ikeji"),
             ],
           ),
+        ),
+        //add bottom navigation bar
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              //active icon
+              activeIcon: Icon(Icons.home_filled, color: Colors.blue),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: "Favorite",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: "Profile",
+            ),
+          ],
+          currentIndex: pageIndex,
+          selectedItemColor: Colors.blue,
+          onTap: (index) {
+            print(index);
+            //set index
+            setState(() {
+              pageIndex = index;
+            });
+          },
         ));
   }
 }
 
-class AdeNewsCard {
-  //return card
-  Widget getCard({title, description, imageUrl, author, date, comments}) {
+//new class for blog list template
+class AdeBlogList {
+  //method
+  Widget getCard({thumbnail, title, description, author, date}) {
     return Card(
-        margin: EdgeInsets.all(10),
-        child: Column(children: [
-          //title
+      margin: EdgeInsets.all(5),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(5, 10, 5, 10),
+        child: Row(children: [
+          //image
           Container(
-            padding: EdgeInsets.fromLTRB(5, 10, 5, 10),
-            child: Row(
-              children: [
-                //image
-                Container(
-                  width: 100,
-                  height: 100,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(5),
-                    image: DecorationImage(
-                      image: NetworkImage(
-                        imageUrl,
-                      ),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        title,
-                        overflow: TextOverflow.ellipsis,
-                        maxLines: 2,
-                        softWrap: true,
-                        style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.black,
-                            height: 1.2,
-                            fontWeight: FontWeight.w900),
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      //date
-                      Text.rich(
-                        TextSpan(
-                          text: "by ",
-                          style: TextStyle(
-                              fontSize: 12,
-                              color: Colors.grey,
-                              height: 1.2,
-                              fontWeight: FontWeight.normal),
-                          children: [
-                            TextSpan(
-                              text: author,
-                              style: TextStyle(
-                                  fontSize: 12,
-                                  color: Colors.black,
-                                  height: 1.2,
-                                  fontWeight: FontWeight.w900),
-                              recognizer: TapGestureRecognizer()
-                                ..onTap = () {
-                                  print("Linda Ikeji");
-                                },
-                            ),
-                            TextSpan(
-                              text: " at $date",
-                              style: TextStyle(
-                                  fontSize: 12,
-                                  color: Colors.grey,
-                                  height: 1.2,
-                                  fontWeight: FontWeight.normal),
-                              recognizer: TapGestureRecognizer()
-                                ..onTap = () {
-                                  print("Comment");
-                                },
-                            ),
-                            TextSpan(
-                                text: " ($comments comments)",
-                                style: TextStyle(
-                                    fontSize: 12,
-                                    color: Colors.grey,
-                                    height: 1.2,
-                                    fontWeight: FontWeight.normal)),
-                          ],
-                        ),
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      Text(
-                        description,
-                        overflow: TextOverflow.ellipsis,
-                        maxLines: 2,
-                        softWrap: true,
-                        style: TextStyle(
-                            fontSize: 14,
-                            color: Colors.grey,
-                            height: 1.2,
-                            fontWeight: FontWeight.normal),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+            width: 100,
+            height: 100,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(thumbnail),
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-        ]));
+          SizedBox(
+            width: 10,
+          ),
+          Expanded(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //title
+              Text(
+                title,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                softWrap: true,
+                style: TextStyle(
+                    fontSize: 16, fontWeight: FontWeight.w900, height: 1.2),
+              ),
+              SizedBox(height: 5),
+              //date and author
+              Text.rich(TextSpan(
+                  text: "by ",
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.grey,
+                    height: 1.2,
+                    fontWeight: FontWeight.normal,
+                  ),
+                  children: [
+                    TextSpan(
+                        text: author,
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.black,
+                          height: 1.2,
+                          fontWeight: FontWeight.w900,
+                        ),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            print("Linda Ikeji");
+                          }),
+                    TextSpan(
+                        text: " at $date",
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.black,
+                          height: 1.2,
+                          fontWeight: FontWeight.normal,
+                        )),
+                    //comment
+                    TextSpan(
+                        text: " (2 Comments)",
+                        style: TextStyle(
+                          fontSize: 10,
+                          color: Colors.grey,
+                          height: 1.2,
+                          fontWeight: FontWeight.normal,
+                        )),
+                  ])),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                description,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                softWrap: true,
+                style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.grey,
+                    height: 1.2,
+                    fontWeight: FontWeight.normal),
+              )
+            ],
+          ))
+        ]),
+      ),
+    );
   }
 }
