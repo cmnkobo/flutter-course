4904fbf0089945d5f8520b61e46e5363c3ff8e25
Adeleye Ayodeji <biodunhi@gmail.com>
2022-09-29 11:39:45 +0100

    Added update

--- lib/pages/shop.dart
+++ lib/pages/shop.dart
@@ -14,144 +14,109 @@ class AdeEcommerce extends StatefulWidget {
 class _AdeEcommerceState extends State<AdeEcommerce> {
   @override
   Widget build(BuildContext context) {
-    double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
 
     return Scaffold(
-        body: Container(
-            padding: EdgeInsets.only(bottom: 0.0),
-            child: Column(
-              children: <Widget>[
-                CarouselSlider(
-                    options: CarouselOptions(
-                      height: (height / 4.2),
-                      enlargeCenterPage: true,
-                      autoPlay: true,
-                      aspectRatio: 16 / 9,
-                      autoPlayInterval: const Duration(seconds: 6),
-                      autoPlayCurve: Curves.fastOutSlowIn,
-                      enableInfiniteScroll: true,
-                      autoPlayAnimationDuration:
-                          const Duration(milliseconds: 800),
-                      viewportFraction: 0.8,
-                    ),
-                    items: [
-                      for (var i = 0; i < 2; i++)
-                        InkWell(
-                          onTap: () {
-                            // Navigator.push(context, MaterialPageRoute(builder: (context) => ProductListView()));
-                          },
-                          child: Container(
-                            width: width,
-                            margin: EdgeInsets.all(5.0),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(5.0),
-                              image: DecorationImage(
-                                image: NetworkImage(
-                                    "https://www.adeleyeayodeji.com/img/IMG_20200522_121756_834_2.jpg"),
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                  gradient: LinearGradient(
-                                begin: Alignment.bottomRight,
-                                colors: [
-                                  Colors.black.withOpacity(1),
-                                  Colors.black.withOpacity(.0),
-                                ],
-                              )),
-                              padding: const EdgeInsets.all(15),
-                              child: Align(
-                                alignment: Alignment.bottomLeft,
-                                child: Container(
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.end,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Container(
-                                        //add a background color
-                                        padding: EdgeInsets.all(4.0),
-                                        decoration: BoxDecoration(
-                                          color: Colors.black.withOpacity(0.5),
-                                          borderRadius:
-                                              BorderRadius.circular(5.0),
-                                        ),
-                                        child: Text(
-                                          "Men Fashion",
-                                          maxLines: 1,
-                                          style: TextStyle(
-                                            color: Colors.white,
-                                            fontSize: 10.0,
-                                            fontFamily: 'Signika Negative',
-                                            fontWeight: FontWeight.w700,
-                                          ),
-                                        ),
-                                      ),
-                                      SizedBox(
-                                        height: 5,
-                                      ),
-                                      Text(
-                                        "Long Sleeve t shirts",
-                                        maxLines: 1,
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 14.0,
-                                          fontFamily: 'Signika Negative',
-                                          fontWeight: FontWeight.w700,
-                                        ),
-                                      ),
-                                      SizedBox(
-                                        height: 5.0,
-                                      ),
-                                      //price
-                                      Row(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.end,
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Text(
-                                            "#5,000",
-                                            maxLines: 1,
-                                            style: TextStyle(
-                                              color: Colors.orange,
-                                              fontSize: 20.0,
-                                              fontFamily: 'Signika Negative',
-                                              fontWeight: FontWeight.w900,
-                                            ),
-                                          ),
-                                          Container(
-                                            //add a background color
-                                            padding: EdgeInsets.all(5.0),
-                                            decoration: BoxDecoration(
-                                              color: Colors.orange,
-                                              borderRadius:
-                                                  BorderRadius.circular(5.0),
-                                            ),
-                                            child: Text(
-                                              "Buy Now",
-                                              maxLines: 1,
-                                              style: TextStyle(
-                                                color: Colors.white,
-                                                fontSize: 10.0,
-                                                fontFamily: 'Signika Negative',
-                                                fontWeight: FontWeight.w700,
-                                              ),
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            ),
+      body: Container(
+          child: CarouselSlider(
+        items: [
+          Container(
+            width: double.infinity,
+            margin: const EdgeInsets.all(5.0),
+            //background image
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: const NetworkImage(
+                    "https://www.adeleyeayodeji.com/img/IMG_20200522_121756_834_2.jpg"),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Container(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                begin: Alignment.bottomRight,
+                colors: [
+                  Colors.black.withOpacity(1),
+                  Colors.black.withOpacity(.0),
+                ],
+              )),
+              child: Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          //add background color
+                          decoration: BoxDecoration(
+                            color: Colors.black.withOpacity(.5),
+                            //border radius
+                            borderRadius: BorderRadius.circular(5),
                           ),
-                        )
-                    ])
-              ],
-            )));
+                          padding: const EdgeInsets.all(3.0),
+                          child: Text(
+                            "Men Fashion",
+                            style: TextStyle(color: Colors.white, fontSize: 13),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          "Long Sleeve t shirts",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w900),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "#5,000",
+                              style: TextStyle(
+                                  color: Colors.orange,
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.w900),
+                            ),
+                            Container(
+                              //add background color
+                              decoration: BoxDecoration(
+                                color: Colors.orange,
+                                //border radius
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                              padding: const EdgeInsets.all(3.0),
+                              child: Text("Buy Now",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 13)),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  )),
+            ),
+          ),
+        ],
+        options: CarouselOptions(
+          height: (height / 4.2),
+          enlargeCenterPage: true,
+          autoPlay: true,
+          aspectRatio: 16 / 9,
+          autoPlayCurve: Curves.fastOutSlowIn,
+          enableInfiniteScroll: true,
+          autoPlayAnimationDuration: Duration(milliseconds: 800),
+          viewportFraction: 0.8,
+        ),
+      )),
+    );
   }
 }
